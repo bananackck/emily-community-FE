3d05953216ac7525ef0ea2f632fe6f6ae1f6b4f0
김부경 <emilly03141221@gmail.com>
2025-02-17 05:36:27 +0900

    (Feat) 로그인 입력 기능 추가

--- Log_in.js
+++ Log_in.js
@@ -1,3 +1,70 @@
-function goSignup(){
+// 이메일 정보
+var elInputEmail = document.getElementById('email')
+var elInputPw = document.getElementById('pw')
+var elLoginBtn = document.getElementById('login-btn')
+// const emailel=document.querySelector("#info-wrap email")
+// helper text
+var elHelperText = document.getElementById('helper-text')
 
+const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+function validEmail(email){
+    if(emailPattern.test(email)===false){return false;}
+    else{return true;}
+}
+function strongPassword(password){
+    if(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]/.test(password)){
+        return true
+    }
+    else{return false}
+}
+function validLength(password){
+    if(password.length>=8 && password.length<=20){
+        return true
+    }
+    else{return false}
+}
+
+function validInfo(){
+    const email=elInputEmail.value
+    const password=elInputPw.value
+
+    if(email=='' || password==''){
+        elHelperText.innerHTML='*이메일 또는 비밀번호를 확인해주세요'
+    }
+    else{
+        elHelperText.innerHTML=''
+    }
+}
+
+elInputEmail.onkeyup=function(){
+    const email=elInputEmail.value
+
+    if(email){
+        if(validEmail(email)==false){
+            elHelperText.innerHTML='*올바른 이메일 주소 형식을 입력해주세요. (예: example@example.com)'
+        }
+        else{
+            elHelperText.innerHTML=''
+        }
+    }
+    else{
+        elHelperText.innerHTML='*이메일을 입력해주세요'
+    }
+}
+elInputPw.onkeyup=function(){
+    const pw=elInputPw.value
+    if(pw){
+        if(validLength(pw)==false){
+            elHelperText.innerHTML='*비밀번호는 8자 이상, 20자 이하이어야 합니다.'
+        }
+        else if(strongPassword(pw)==false){
+            elHelperText.innerHTML='*대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.'
+        }
+        else{
+            elHelperText.innerHTML=''
+        }
+    }
+    else{
+        elHelperText.innerHTML='*비밀번호를 입력해주세요'
+    }
 }
