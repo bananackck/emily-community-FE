a44bcb21bae8ca6469de17a315c16b8012a66475
김부경 <emilly03141221@gmail.com>
2025-02-18 10:18:04 +0900

    (Feat) 게시글 작성 구현

---
+++ Make_post.js
@@ -0,0 +1,55 @@
+var elInputTitle = document.getElementById('title')
+var elInputText = document.getElementById('text')
+var elCompleteBtn = document.getElementById('complete-btn')
+var elTextHelper = document.getElementById('text-helper')
+var elFile = document.getElementById('file')
+
+var titlePass = false
+var textPass = false
+
+const info=document.getElementById('info-wrap')
+
+elInputTitle.onkeyup=function(){
+    const title=elInputTitle.value
+    titlePass=false
+    if(title){
+        titlePass=true
+    }
+    btnActivate()
+} 
+elInputText.onkeyup=function(){
+    const text=elInputText.value
+    textPass=false
+    if(text){
+        elTextHelper.innerHTML=''
+        textPass=true
+    }
+    else{
+        elTextHelper.innerHTML='*제목, 내용을 모두 작성해주세요'
+    }
+    console.log(elTextHelper)
+    btnActivate()
+}
+elFile.on
+elFile.onchange=function(){
+    const selected=this.files[0];
+
+    const reader = new FileReader();
+    
+    reader.onload=function(){
+        var msg=document.getElementById('filename')
+        msg.innerHTML=selected.name
+    }
+    reader.readAsDataURL(selected)
+}
+function btnActivate(){
+    if(titlePass && textPass){
+        elCompleteBtn.style.backgroundColor="#7F6AEE"
+        elCompleteBtn.onclick=function(){
+            window.location.href="./Posts.html"
+        }
+    }
+    else{
+        elCompleteBtn.style.backgroundColor="#ACA0EB"
+    }
+}
