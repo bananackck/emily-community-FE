bc982d460162e020d1b31fde2c6d48799f6c6c50
김부경 <emilly03141221@gmail.com>
2025-03-03 04:47:13 +0900

    (Feat) 헤더 nav 버튼 on/off

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,16 @@
 const template = document.createElement('template');
 template.innerHTML = `
-  <link rel="stylesheet" type="text/css" href="../assets/style/Header.css">
+  <link rel="stylesheet" type="text/css" href="../assets/style/header.css">
   <header>
     <div class="header-contents">    
-        <div class="nav-btn left hidden">
-            <a href="./Log_in.html">
-                <img id="nav-back-img" src="../assets/img/navigate_back.png" alt="뒤로가기">
+        <div class="nav-btn left">
+            <a href="./login.html">
+                <img id="nav-back-img" src="../assets/img/navigate-back.png" alt="뒤로가기">
             </a>
         </div>
         <div class="project-name">아무 말 대잔치</div>
-        <div class="nav-btn right hidden">
-            <a href="./Posts.html">
+        <div class="nav-btn right">
+            <a href="./posts.html">
                 <img id="profile-img" src="../assets/img/profile.png" alt="프로필 가기" style="border-radius: 20px;">
             </a>
         </div>
@@ -24,6 +24,18 @@ class MyHeader extends HTMLElement {
     // Shadow DOM 사용: 스타일 및 DOM 캡슐화 가능
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    // 만약 "visible-nav" 속성이 있으면 네비 버튼에서 hidden 클래스를 제거
+    if (this.hasAttribute('left-hidden')) {
+      const navBtns = this.shadowRoot.querySelectorAll('.nav-btn.left');
+      navBtns.forEach(btn => btn.classList.add('hidden'));
+    }
+    if (this.hasAttribute('right-hidden')) {
+      const navBtns = this.shadowRoot.querySelectorAll('.nav-btn.right');
+      navBtns.forEach(btn => btn.classList.add('hidden'));
+    }
   }
 }
 
