b920b287cc47f6926f85f499d53b981c7cc9e89d
김부경 <emilly03141221@gmail.com>
2025-03-03 05:37:53 +0900

    (Refact) validator 컴포넌트 분리

---
+++ src/components/validator.js
@@ -0,0 +1,91 @@
+const emailPattern = /^[A-Za-z0-9_.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+
+export const validEmail = email =>
+  emailPattern.test(email);
+
+export const strongPassword = password =>
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]/.test(password);
+
+export const validLength = password =>
+  password.length >= 8 && password.length <= 20;
+
+export const pwValidator = (el, helper) => {
+  if (!validLength(el)) {
+    helper.innerHTML = '*비밀번호는 8자 이상, 20자 이하이어야 합니다.';
+    return false;
+  } else if (!strongPassword(el)) {
+    helper.innerHTML = '*대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.';
+    return false;
+  }
+  return true;
+};
+
+export const handleEmailInput = (email, elEmailHelper) => {
+  if (email) {
+    if (!validEmail(email)) {
+      elEmailHelper.innerHTML =
+        '*올바른 이메일 주소 형식을 입력해주세요.<br>(예: example@example.com)';
+      return false;
+    } else {
+      // if(overlappingEmail()){
+      //     elEmailHelper.innerHTML='*중복된 이메일입니다.'
+      // }
+      // else{return true}
+      elEmailHelper.innerHTML = '';
+      return true;
+    }
+  } else {
+    elEmailHelper.innerHTML = '*이메일을 입력해주세요.';
+    return false;
+  }
+};
+
+export const handlePwInput = (pw, elPwHelper) => {
+  if (pw) {
+    if (pwValidator(pw, elPwHelper)) {
+      elPwHelper.innerHTML = '';
+      return true;
+    }
+    return false;
+  } else {
+    elPwHelper.innerHTML = '*비밀번호를 입력해주세요.';
+    return false;
+  }
+};
+
+export const handlePw2Input = (pw, pw2, elPw2Helper) => {
+  if (pw2) {
+    if (pwValidator(pw2, elPw2Helper)) {
+      // 주의: 아래 pw 변수는 함수 내부에 정의되지 않은 전역변수일 수 있으므로, 필요에 따라 수정해야 합니다.
+      if (pw != pw2) {
+        elPw2Helper.innerHTML = '*비밀번호가 다릅니다.';
+        return false;
+      } else {
+        elPw2Helper.innerHTML = '';
+        return true;
+      }
+    }
+    return false;
+  } else {
+    elPw2Helper.innerHTML = '*비밀번호를 한 번 더 입력해주세요.';
+    return false;
+  }
+};
+
+export const handleNicknameInput = (nickname, elNicknameHelper) => {
+  if (nickname) {
+    if (nickname.includes(' ')) {
+      elNicknameHelper.innerHTML = '*띄어쓰기를 없애주세요.';
+      return false;
+    } else if (nickname.length >= 11) {
+      elNicknameHelper.innerHTML = '*닉네임은 최대 10자까지 작성 가능합니다.';
+      return false;
+    } else {
+      elNicknameHelper.innerHTML = '';
+      return true;
+    }
+  } else {
+    elNicknameHelper.innerHTML = '*닉네임을 입력해주세요.';
+    return false;
+  }
+};
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,3 +1,5 @@
+import * as validator from "../components/validator.js"
+
 var elInputProfile = document.getElementById('profile-image')
 var elInputEmail = document.getElementById('email')
 var elInputPw = document.getElementById('pw')
@@ -19,23 +21,7 @@ var elNicknameHelper = document.getElementById('nickname-helper')
 const info=document.getElementById('info-wrap')
 var elSigninBtn = document.getElementById('signin-btn')
 
-const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-function validEmail(email){
-    if(emailPattern.test(email)===false){return false;}
-    else{return true;}
-}
-function strongPassword(password){
-    if(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]/.test(password)){
-        return true
-    }
-    else{return false}
-}
-function validLength(password){
-    if(password.length>=8 && password.length<=20){
-        return true
-    }
-    else{return false}
-}
+
 
 elInputProfile.onchange = function(){
     profilePass=false
@@ -53,105 +39,37 @@ elInputProfile.onchange = function(){
     reader.readAsDataURL(selected)
 }
 
-elInputEmail.onkeyup=function(){
-    const email=elInputEmail.value
-    emailPass=false
-    if(email){
-        if(validEmail(email)==false){
-            elEmailHelper.innerHTML='*올바른 이메일 주소 형식을 입력해주세요.</br>(예: example@example.com)'
-        }
-        else{
-            // if(overlappingEmail()){
-            //     elEmailHelper.innerHTML='*중복된 이메일입니다.'
-            // }
-            // else{return true}
-            elEmailHelper.innerHTML=''
-            emailPass=true
-        }
-    }
-    else{
-        elEmailHelper.innerHTML='*이메일을 입력해주세요.'
-    }
-    btnActivate()
-}
-function pwValidator(el, helper){
-    if(validLength(el)==false){
-        helper.innerHTML='*비밀번호는 8자 이상, 20자 이하이어야 합니다.'
-        return false
-    }
-    else if(strongPassword(el)==false){
-        helper.innerHTML='*대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.'
-        return false
-    }
-    return true
-}
+elInputEmail.onkeyup = function() {
+    emailPass = validator.handleEmailInput(elInputEmail.value, elEmailHelper);
+    btnActivate();
+};
+
+
 elInputPw.onkeyup=function(){
-    const pw=elInputPw.value
-    const pw2=elInputPw2.value
-    pwPass=false
-    if(pw){
-        if(pwValidator(pw, elPwHelper)){
-            elPwHelper.innerHTML=''
-            pwPass=true
-        }
-    }
-    else{
-        elPwHelper.innerHTML='*비밀번호를 입력해주세요.'
-    }
+    pwPass=validator.handlePwInput(elInputPw.value, elPwHelper);
     btnActivate()
-}
+};
+
+
 elInputPw2.onkeyup=function(){
-    const pw=elInputPw.value
-    const pw2=elInputPw2.value
-    pw2Pass=false
-    if(pw2){
-        if(pwValidator(pw2, elPw2Helper)){
-            if(pw!=pw2){
-                elPw2Helper.innerHTML='*비밀번호가 다릅니다.'
-            }
-            else{
-            elPw2Helper.innerHTML=''
-            pw2Pass=true
-        }
-        }
-    }
-    else{
-        elPw2Helper.innerHTML='*비밀번호를 한 번 더 입력해주세요.'
-    }
-    btnActivate()
-}
+    pw2Pass=validator.handlePw2Input(elInputPw.value, elInputPw2.value, elPw2Helper);
+    btnActivate();
+};
+
 elInputNickname.onkeyup=function(){
-    const nickname=elInputNickname.value
-    nicknamePass=false
-    if(nickname){
-        if(nickname.includes(" ")){
-            elNicknameHelper.innerHTML='*띄어쓰기를 없애주세요.'
-        }
-        else if(nickname.length>=11){
-            elNicknameHelper.innerHTML='*닉네임은 최대 10자까지 작성 가능합니다.'
-        }
-        // else if(overlappingNickname()){
-        //     elNicknameHelper.innerHTML='*중복된 닉네임입니다.'
-        // }
-        else{
-            elNicknameHelper.innerHTML=''
-            nicknamePass=true
-        }
-    }
-    else{
-        elNicknameHelper.innerHTML='*닉네임을 입력해주세요.'
-    }
+    nicknamePass=validator.handleNicknameInput(elInputNickname.value, elNicknameHelper);
     btnActivate()
-}
+};
+
 
 function btnActivate(){
-    if(profilePass && emailPass && pwPass && pw2Pass && nicknamePass){
-        elSigninBtn.style.backgroundColor="#7F6AEE"
+    if(emailPass && pwPass && pw2Pass && nicknamePass){
+        elSigninBtn.style.backgroundColor="var(--activate-color)"
         elSigninBtn.onclick=function(){
-            window.location.href="./Posts.html"
+            window.location.href="./posts.html"
         }
     }
     else{
-        elSigninBtn.style.backgroundColor="#ACA0EB"
+        elSigninBtn.style.backgroundColor="var(--point-color)"
     }
 }
